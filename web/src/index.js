b0cd9d40fd5b6c43a47d80ab2789ff3c9bbefa0d
Yasuaki Matsuda <yasuaki.matsuda@iridge.jp>
2017-06-22 13:39:27 +0900

    Change the domain to web.

--- web/src/index.js
+++ web/src/index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const app = express();
 
 app.get('/', (req, res) => {
-  axios.get('http://localhost:3000/').then(({data}) => {
+  axios.get('http://app:3000/').then(({data}) => {
     res.send(`
       <h1>${data.text}</h1>
     `);
